ee5e6effc846702cb71622499c7da9d20225b350
山村歩 <ayugooayu@gmail.com>
2020-05-20 23:06:59 +0900

    新規作成

---
+++ SimpleMemo/app/src/main/java/com/example/simplememo/CreateMemo.java
@@ -0,0 +1,14 @@
+package com.example.simplememo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateMemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_memo);
+    }
+}
