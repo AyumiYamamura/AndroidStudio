4c7c0ea2afaf9f7f918a1cccebac7afce58caff0
山村歩 <ayugooayu@gmail.com>
2020-06-25 22:54:38 +0900

    更新

--- MemoKotlin/app/src/main/java/com/example/memokotlin/CreateKotlinMemo.kt
+++ MemoKotlin/app/src/main/java/com/example/memokotlin/CreateKotlinMemo.kt
@@ -1,11 +1,60 @@
 package com.example.memokotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class CreateKotlinMemo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_kotlin_memo)
+
+
+        // ListActivityからインテントを取得
+        val intent = this.intent
+        // 値を取得
+        val id = intent.getStringExtra("id")
+        // 画面に表示
+        if (id == "") {
+            // 新規作成の場合
+
+        } else {
+            // 編集の場合
+
+        }
+
+        /**
+         * 登録ボタン処理
+         */
+        // idがregisterのボタンを取得
+        val registerButton = findViewById<View>(R.id.register) as Button
+        // clickイベント追加
+        registerButton.setOnClickListener {
+            // 入力内容を取得する
+            val body = findViewById<View>(R.id.body) as EditText
+            val bodyStr = body.text.toString()
+
+            // ToDo:データベースに保存する
+
+            // 保存後に一覧へ戻る
+            val intent = Intent(this@CreateKotlinMemo,  com.example.memokotlin.MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        /**
+         * 戻るボタン処理
+         */
+        // idがbackのボタンを取得
+        val backButton = findViewById<View>(R.id.back) as Button
+        // clickイベント追加
+        backButton.setOnClickListener {
+            // 保存せずに一覧へ戻る
+            finish()
+        }
     }
+
 }
--- MemoKotlin/app/src/main/java/com/example/memokotlin/MainActivity.kt
+++ MemoKotlin/app/src/main/java/com/example/memokotlin/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.memokotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ListView
-import android.widget.SimpleAdapter
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_create_kotlin_memo.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,24 +14,61 @@ class MainActivity : AppCompatActivity() {
 
         // ToDo:データベースから値を取得する
         // 仮のデータを作成
-        val tmplist = ArrayList<HashMap<String, String>>()
+        val tmpList = ArrayList<HashMap<String, String>>()
         for (i in 1..5) {
             val data = HashMap<String, String>()
-            // 引数には、(名前,実際の値)という組合せで指定.　名前はSimpleAdapterの引数で使用
+            // 引数には、(名前,実際の値)という組合せで指定します　名前はSimpleAdapterの引数で使用します
             data["body"] = "サンプルデータ$i"
             data["id"] = "sampleId$i"
-            tmplist.add(data)
+            tmpList.add(data)
         }
+
         // Adapter生成
         // ToDo:tmpListを正式なデータと入れ替える
         val simpleAdapter = SimpleAdapter(this,
-                tmplist,// 使用するデータ
-                android.R.layout.simple_list_item_2, // 使用するレイアウト
-                arrayOf("body", "id"), // どの項目を
-                intArrayOf(android.R.id.text1, android.R.id.text2) // どのidの項目に入れるか
+            tmpList, // 使用するデータ
+            android.R.layout.simple_list_item_2, // 使用するレイアウト
+            arrayOf("body", "id"), // どの項目を
+            intArrayOf(android.R.id.text1, android.R.id.text2) // どのidの項目に入れるか
         )
-        // idがmemoListのListViewを取得
-        val listview =findViewById<View>(R.id.memoList) as ListView
 
+        // idがmemoListのListViewを取得
+        val listView = findViewById<View>(R.id.memoList) as ListView
+        listView.adapter = simpleAdapter
+
+        // リスト項目をクリックした時の処理
+        listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            /**
+             * @param parent ListView
+             * @param view 選択した項目
+             * @param position 選択した項目の添え字
+             * @param id 選択した項目のID
+             */
+            // インテント作成  第二引数にはパッケージ名からの指定で、遷移先クラスを指定
+            val intent = Intent(this@MainActivity, com.example.memokotlin.CreateKotlinMemo::class.java)
+
+            // 選択されたビューを取得 TwoLineListItemを取得した後、text2の値を取得する
+            val two = view as TwoLineListItem
+            val idTextView = two.text2 as TextView
+            val isStr = idTextView.text as String
+            // 値を引き渡す (識別名, 値)の順番で指定します
+            intent.putExtra("id", isStr)
+            // Activity起動
+            startActivity(intent)
+        }
+
+
+        /**
+         * 新規作成するボタン処理
+         */
+        // idがnewButtonのボタンを取得
+        val newButton = findViewById<View>(R.id.newButton) as Button
+        // clickイベント追加
+        newButton.setOnClickListener {
+            // CreateMemoActivityへ遷移
+            val intent = Intent(this@MainActivity, com.example.memokotlin.CreateKotlinMemo::class.java)
+            intent.putExtra("id", "")
+            startActivity(intent)
+        }
     }
 }
